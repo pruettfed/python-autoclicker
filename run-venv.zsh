9ea3b0785e41e2ce04a897589ea49f7caff6a02a
pruettfed <pruettfed@gmail.com>
2022-09-06 17:28:18 -0500

    Added windows support for running in a venv

--- run-venv.zsh
+++ run-venv.zsh
@@ -6,9 +6,24 @@
 DIR=$(dirname "$0")
 # echo "$DIR" --> returns ~/.../.../Autoclicker
 
-#activate venv
-source $DIR/.venv/bin/activate
-python3 $DIR/autoclicker.py
+### =>> MAC SYSTEMS <<= ###
+if [[ "$OSTYPE" == "darwin"* ]]; then
+    #activate venv
+    source $DIR/.venv/bin/activate
+    python3 $DIR/autoclicker.py
+### =>> MAC SYSTEMS <<= ###
+
+### =>> WINDOWS SYSTEMS <<= ###
+elif [[ "$OSTYPE" == "msys" || "$OSTYPE" == "cygwin" ]]; then
+    #activate venv
+    source $DIR/.venv/Scripts/activate
+    python3.exe $DIR/autoclicker.py
+### =>> WINDOWS SYSTEMS <<= ###
+
+else
+    echo "ERROR: Unknown system"
+fi
+
 
 #on autoclicker being killed deactivate venv
 deactivate
